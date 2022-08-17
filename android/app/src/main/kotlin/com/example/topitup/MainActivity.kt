500467fe73c0399fca4d76109ed3fc9ad2c958f1
Abdulazeez <abdulazeezoshinowo2@gmail.com>
2022-08-17 15:50:47 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/topitup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.topitupng.topitupng
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const TopitupNg(),
+  );
+}
+
+class TopitupNg extends StatelessWidget {
+  const TopitupNg({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TopitupNG',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Topitup NG',
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Topitup Ng',
+        ),
+      ),
+    );
+  }
+}
