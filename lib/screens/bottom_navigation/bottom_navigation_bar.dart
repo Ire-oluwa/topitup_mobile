618b7ec4d0724b925a3eba23d8acc70834537d68
Abdulazeez <abdulazeezoshinowo2@gmail.com>
2022-09-08 19:49:16 +0100

    ft(data-airtime): implement data and airtime feature

--- lib/screens/bottom_navigation/bottom_navigation_bar.dart
+++ lib/screens/bottom_navigation/bottom_navigation_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+
 import '../../constants/app_constants.dart';
 import '../cable/cable_screen.dart';
 import '../dashboard/dashboard_screen.dart';
--- lib/screens/dashboard/components/dashboard_header_transaction_details_card.dart
+++ lib/screens/dashboard/components/dashboard_header_transaction_details_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 import '../../../constants/app_constants.dart';
 import '../../../constants/palette.dart';
 import '../../components/custom_text.dart';
